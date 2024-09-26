c4c3e9b5d3ae31a26b9f8b51e7d1467a3a86f5fd
MansiP10 <mansi.prajapati101998@gmail.com>
2024-09-26 22:09:37 +0530

    js task 12 done

---
+++ js-task12/script.js
@@ -0,0 +1,32 @@
+let number1 = 7;
+let number2 = 17;
+let number3 = 20;
+let number4 = 42;
+
+if(number1 % 2 == 0){
+    console.log(`The number ${number1} is even`);
+}
+else{
+    console.log(`The number ${number1} is odd`);
+}
+
+if(number2 % 2 == 0){
+    console.log(`The number ${number2} is even`);
+}
+else{
+    console.log(`The number ${number2} is odd`);
+}
+
+if(number3 % 2 == 0){
+    console.log(`The number ${number3} is even`);
+}
+else{
+    console.log(`The number ${number3} is odd`);
+}
+
+if(number4 % 2 == 0){
+    console.log(`The number ${number4} is even`);
+}
+else{
+    console.log(`The number ${number4} is odd`);
+}
