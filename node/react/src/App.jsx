d333be36c6d5196d87e39b0046851c94469726a5
MansiP10 <mansi.prajapati101998@gmail.com>
2024-10-23 22:52:51 +0530

    baground changesr

--- node/react/src/App.jsx
+++ node/react/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ToggleBg from './Toggle'
+import ToggleBg from './ToggleBg'
 
 function App() {
   return (
